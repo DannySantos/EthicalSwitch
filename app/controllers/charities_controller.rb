3d7b3c60c83bfe548a357344742a6036bac90c8f
Danny Santos <danny@ethicalswitch.com>
2017-07-31 15:33:01 +0100

    Production fixes

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,7 +1,9 @@
 class CharitiesController < ApplicationController
   def show
     @charity = Charity.find(params[:id])
+    @charities = Charity.all.current
     assign_partner_variables
     @faqs = Faq.all
+    @switch = Switch.new
   end
 end
